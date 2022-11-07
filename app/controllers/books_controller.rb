54af5c772fa33169191a15663c85b002e99c0344
EC2 Default User <ec2-user@ip-172-31-24-39.ec2.internal>
2022-11-07 09:59:13 +0000

    [Finish] 3-25

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,68 @@
+class BooksController < ApplicationController
+
+  def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+    @books = Book.all
+    @user = @book.user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @user = @book.user
+    if @user.id != current_user.id
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
+  end
+
+  # 投稿データの保存
+  def create
+    @user = current_user
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id]) # データの取得(idを用いて1件ずつ)
+    if book.destroy # データの削除
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+    else
+      render :index
+    end
+  end
+
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+
+  # name: 一意性 & 2~20文字
+  validates :name, uniqueness: true, length: {in: 2..20}
+  # introduction: 最大50文字
+  validates :introduction, length: { maximum: 50}
+
+  has_one_attached :profile_image
+
+  def get_profile_image(height, width)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
+
 end
